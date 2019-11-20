1a7692be3ed84812be628959840dc0a6a85bc1ce
GlennKimbleJr <glennkimble@yahoo.com>
2019-11-19 20:48:22 -0500

    Setup jwt driver for auth.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,11 +2,18 @@
 
 namespace App\Providers;
 
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Exception;
+use App\Models\User;
+use App\Traits\InteractsWithJWT;
+use Firebase\JWT\ExpiredException;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
+    use InteractsWithJWT;
+
     /**
      * The policy mappings for the application.
      *
@@ -25,6 +32,30 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        $this->registerAuthGuard();
+    }
+
+    /**
+     * @return void
+     */
+    private function registerAuthGuard(): void
+    {
+        Auth::viaRequest('jwt', function ($request) {
+            $token = $request->bearerToken();
+            abort_if(! $token, 401, 'Token not provided.');
+
+            try {
+                $credentials = $this->decodeJWT($token);
+            } catch(ExpiredException $e) {
+                abort(419, 'Provided token is expired.');
+            } catch(Exception $e) {
+                abort(400, 'An error while decoding token.');
+            }
+
+            $user = User::find($credentials->sub);
+            abort_if(! $user, 400, 'Email or password is wrong.');
+
+            return $user;
+        });
     }
 }
